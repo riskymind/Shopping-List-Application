490cf758b0213a54317e19db3bea449c455cab96
kelechi <oparaiskelechi@gmail.com>
2021-02-09 02:46:31 +0100

    implement dependency injection using kodein on the application

--- app/src/main/java/com/example/shoppinglist/ui/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ui/MainActivity.kt
@@ -7,19 +7,26 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shoppinglist.R
 import com.example.shoppinglist.adapter.ShoppingAdapter
-import com.example.shoppinglist.model.db.ShoppingDatabase
 import com.example.shoppinglist.model.db.entities.ShoppingItem
-import com.example.shoppinglist.model.repository.ShoppingRepository
 import kotlinx.android.synthetic.main.activity_main.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.generic.instance
+import org.kodein.di.android.kodein
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), KodeinAware {
+
+    override val kodein by kodein()
+
+
+    private val factory: ShoppingViewModelFactory by instance()
+    lateinit var viewModel: ShoppingViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val database = ShoppingDatabase(this)
-        val repository = ShoppingRepository(database)
-        val factory = ShoppingViewModelFactory(repository)
-        val viewModel = ViewModelProvider(this, factory).get(ShoppingViewModel::class.java)
+
+        viewModel = ViewModelProvider(this, factory).get(ShoppingViewModel::class.java)
 
         val adapter = ShoppingAdapter(this, listOf(), viewModel)
         rvShoppingItems.layoutManager = LinearLayoutManager(this)
@@ -39,4 +46,7 @@ class MainActivity : AppCompatActivity() {
             }).show()
         }
     }
+
+
+
 }
